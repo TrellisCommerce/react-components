5570d23aa42165f04c840e6228344ec9da052cf3
Loan Laux <llaux@trellis.co>
2022-02-08 18:07:38 +0400

    Set up Tailwind 3 + rename TestComponent to CallToAction
    Signed-off-by: Loan Laux <llaux@trellis.co>

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,38 @@
+// .storybook/main.js
+
+const path = require('path');
+
 module.exports = {
-  stories: [
-    '../stories/**/*.stories.mdx',
-    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
+  stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    {
+      /**
+       * Fix Storybook issue with PostCSS@8
+       * @see https://github.com/storybookjs/storybook/issues/12668#issuecomment-773958085
+       */
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
   ],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-  framework: '@storybook/react',
+  core: {
+    builder: 'webpack5',
+  },
+  webpackFinal: (config) => {
+    /**
+     * Add support for alias-imports
+     * @see https://github.com/storybookjs/storybook/issues/11989#issuecomment-715524391
+     */
+    config.resolve.alias = {
+      ...config.resolve?.alias,
+      '@': [path.resolve(__dirname, '../lib/'), path.resolve(__dirname, '../')],
+    };
+
+    return config;
+  },
 };
--- stories/CallToAction.stories.jsx
+++ stories/CallToAction.stories.jsx
@@ -1,43 +1,43 @@
 import React from 'react';
 
-import { TestComponent } from '/index.js';
+import { CallToAction } from '/index.js';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'TestComponent',
-  component: TestComponent,
+  title: 'CallToAction',
+  component: CallToAction,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
-    imageAltText: { control: 'text' },
-    imageSrc: { control: 'text' },
-    titleText: { control: 'text' },
-    paragraphText: { control: 'text' },
+    imageAltText: { control: 'text', defaultValue: 'This is an image' },
+    imageSrc: { control: 'text', defaultValue: 'https://via.placeholder.com/150' },
+    titleText: { control: 'text', defaultValue: 'Hello World' },
+    paragraphText: { control: 'text', defaultValue: 'Lorem ipsum dolor sit amet' },
   },
 };
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template = (args) => <TestComponent {...args} />;
+const Template = (args) => <CallToAction {...args} />;
 
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
   primary: true,
-  label: 'TestComponent',
+  label: 'CallToAction',
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
-  label: 'TestComponent',
+  label: 'CallToAction',
 };
 
 export const Large = Template.bind({});
 Large.args = {
   size: 'large',
-  label: 'TestComponent',
+  label: 'CallToAction',
 };
 
 export const Small = Template.bind({});
 Small.args = {
   size: 'small',
-  label: 'TestComponent',
+  label: 'CallToAction',
 };
