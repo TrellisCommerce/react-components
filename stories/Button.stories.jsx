6ae4de18fe61fb3f06381ec870a54796548cfcd5
Loan Laux <llaux@trellis.co>
2022-02-21 19:10:27 +0400

    add Button component with classnames utility
    Signed-off-by: Loan Laux <llaux@trellis.co>

---
+++ stories/Button.stories.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { Button } from '/index.js';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Button',
+  component: Button,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    disabled: { control: 'boolean', defaultValue: false },
+    displayText: { control: 'text', defaultValue: 'Add to Cart' },
+  },
+};
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  primary: true,
+  label: 'Button',
+};
