20912f56e5a539194bd4a11b50c189560067a405
Ahmed Bennani <alahmambennani@trellis.co>
2022-03-17 14:39:30 +0100

    redirect home to /storybook-static

--- pages/api/home.js
+++ pages/api/home.js
@@ -1,7 +1,3 @@
-import fs from 'fs';
-
 export default async function api(req, res) {
-  res.setHeader('Content-Type', 'text/html; charset=utf-8');
-  res.write(await fs.readFileSync('./public/storybook-static/index.html', 'utf-8'));
-  res.end();
+  res.redirect('/storybook-static');
 }
