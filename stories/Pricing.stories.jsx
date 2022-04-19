47f3b13f5b1fc9517291d39d3d405853fd4187e0
Loan Laux <llaux@trellis.co>
2022-04-19 17:00:18 +0400

    TECH-526: match designs on CartSummary for desktop
    Signed-off-by: Loan Laux <llaux@trellis.co>

--- stories/Pricing.stories.jsx
+++ stories/Pricing.stories.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import SSR from "../lib/SSR";
+import SSR from '../lib/SSR';
 
 import { Pricing } from '/index.js';
 
 export default {
   title: 'Pricing',
   component: Pricing,
-	description: 'Pricing page',
+  description: 'Pricing page',
   argTypes: {
     originalPrice: { control: 'text', defaultValue: '$99.99' },
     promotionalPrice: { control: 'text', defaultValue: '$49.99' },
@@ -15,4 +15,4 @@ export default {
 
 export const Primary = (args) => <Pricing {...args} />;
 
-export const Ssr = (args) => <SSR {...args} component={'Pricing'} />
+export const Ssr = (args) => <SSR {...args} component={'Pricing'} />;
