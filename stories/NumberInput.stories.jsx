5848545289c1dd2b391c63a3ea648211b7141439
Kelly Audu <kaudu@trellis.co>
2022-04-25 09:38:10 +0100

    =Added state management to component story

--- stories/NumberInput.stories.jsx
+++ stories/NumberInput.stories.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SSR from "../lib/SSR";
 
 import { NumberInput } from '/index.js';
@@ -15,7 +15,20 @@ export default {
 };
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template = (args) => <NumberInput {...args} />;
+export const Template = (args) => {
+  const [quantity, setQuantity] = useState(1);
+  const onChange = (elementId, quantity) => {
+   setQuantity(quantity)
+  };
+  return (
+    <NumberInput
+      {...args}
+      value={quantity}
+      onChange={onChange}
+    />
+  )
+
+};
 
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
