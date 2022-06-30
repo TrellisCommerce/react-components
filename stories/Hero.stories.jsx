351e29c07381e22dc64690bca8579539d5aa439a
Kelly Audu <kaudu@trellis.co>
2022-06-30 02:16:21 +0100

    =added proptypes to hero component

--- stories/Hero.stories.jsx
+++ stories/Hero.stories.jsx
@@ -58,5 +58,6 @@ Carousel.args = {
       subTitle: "Discover our latest women's releases in celebration of Women's History Month",
       imageUrl: "https://images.unsplash.com/photo-1469334031218-e382a71b716b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80"
     }
-  ]
+  ],
+  NextImage: Image
 };
