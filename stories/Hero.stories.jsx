02f91e12e5d3ddfe0024bb1e3bc1534c1b9b9edd
Kelly Audu <kaudu@trellis.co>
2022-07-27 03:32:54 +0100

    :=TECH-438 PR changes

--- stories/Hero.stories.jsx
+++ stories/Hero.stories.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import Image from 'next/image';
 
-
-import { Button as ButtonComponent, Hero } from "/index.js";
+import { Button as ButtonComponent, Hero } from '/index.js';
 const Button = () => {
-  return (<ButtonComponent
-    displayText="Shop Now"
-    onClick={() => {
-      alert('You clicked me');
-    }}
-  />)
-}
+  return (
+    <ButtonComponent
+      displayText="Shop Now"
+      onClick={() => {
+        alert('You clicked me');
+      }}
+    />
+  );
+};
 /**
  * Next Image loader
  * @returns {*}
  */
 const myLoader = (src) => {
-  return src
+  return src;
 };
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
@@ -30,9 +31,8 @@ export default {
         title: "Women's History Month",
         subTitle:
           "Discover our latest women's releases in celebration of Women's History Month",
-        imageUrl:
-          'https://source.unsplash.com/random/1600x900',
-      }
+        imageUrl: 'https://source.unsplash.com/random/1600x900',
+      },
     ],
     hasOverlay: { control: 'boolean', defaultValue: false },
   },
@@ -41,9 +41,9 @@ export default {
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template = (args) => <Hero {...args} />;
 
-export const Primary = Template.bind({});
+export const SinglePrimary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
+SinglePrimary.args = {
   label: 'Hero',
   Button,
   slides: [
@@ -51,30 +51,10 @@ Primary.args = {
       title: "Women's History Month",
       subTitle:
         "Discover our latest women's releases in celebration of Women's History Month",
-      imageUrl:
-        'https://source.unsplash.com/random/1600x900',
-    }
+      imageUrl: 'https://source.unsplash.com/random/1600x900',
+    },
   ],
 };
-
-export const NextImage = Template.bind({});
-  NextImage.args = {
-     label: 'Hero',
-    Button,
-    slides: [
-      {
-        title: "Women's History Month",
-        subTitle:
-          "Discover our latest women's releases in celebration of Women's History Month",
-        imageComponent: () => (
-          <Image loader={() => 'https://images.unsplash.com/photo-1469334031218-e382a71b716b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80'}
-                 src="https://images.unsplash.com/photo-1469334031218-e382a71b716b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80"
-                 layout="fill"
-                 objectFit="cover"/>
-        ),
-      }
-    ]
-  };
 
 export const Carousel = Template.bind({});
 Carousel.args = {
@@ -85,8 +65,7 @@ Carousel.args = {
       title: "Women's History Month",
       subTitle:
         "Discover our latest women's releases in celebration of Women's History Month",
-      imageUrl:
-        'https://source.unsplash.com/random/1600x900',
+      imageUrl: 'https://source.unsplash.com/random/1600x900',
     },
     {
       title: "Women's History Month",
@@ -99,8 +78,29 @@ Carousel.args = {
       title: "Women's History Month",
       subTitle:
         "Discover our latest women's releases in celebration of Women's History Month",
-      imageUrl:
-        'https://source.unsplash.com/random/1600x900',
+      imageUrl: 'https://source.unsplash.com/random/1600x900',
     },
-  ]
+  ],
+};
+export const CustomImageComponent = Template.bind({});
+CustomImageComponent.args = {
+  label: 'Hero',
+  Button,
+  slides: [
+    {
+      title: "Women's History Month",
+      subTitle:
+        "Discover our latest women's releases in celebration of Women's History Month",
+      imageComponent: () => (
+        <Image
+          loader={() =>
+            'https://images.unsplash.com/photo-1469334031218-e382a71b716b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80'
+          }
+          src="https://images.unsplash.com/photo-1469334031218-e382a71b716b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80"
+          layout="fill"
+          objectFit="cover"
+        />
+      ),
+    },
+  ],
 };
